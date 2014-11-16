6445772245b20560c36bdb1a6131a5ac292f324d
Jc2502 <juank2acosta@gmail.com>
2014-11-16 01:53:18 -0600

    Nav Drawer

--- app/src/main/java/com/pathofthefood/flyingburger/Address/AddressBook.java
+++ app/src/main/java/com/pathofthefood/flyingburger/Address/AddressBook.java
@@ -5,34 +5,53 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 import com.melnykov.fab.FloatingActionButton;
 import com.pathofthefood.flyingburger.CONFIG;
 import com.pathofthefood.flyingburger.HttpClientHelp;
 import com.pathofthefood.flyingburger.NotAuthException;
 import com.pathofthefood.flyingburger.R;
 import com.pathofthefood.flyingburger.SharedPerferencesObjects;
+import com.pathofthefood.flyingburger.ldrawer_library.ActionBarDrawerToggle;
+import com.pathofthefood.flyingburger.ldrawer_library.DrawerArrowDrawable;
 import com.pathofthefood.flyingburger.utils.SessionManager;
 import org.json.JSONException;
 
 import java.util.ArrayList;
 
-public class AddressBook extends Activity {
+public class AddressBook extends Activity implements AdapterView.OnItemClickListener {
 
     FloatingActionButton address_add;
     private ListView AddressList;
     private ArrayList<Address> addressess;
     private AddressAdapter adapter;
     private SessionManager session;
+    private ArrayAdapter<String> navigationDrawerAdapter;
+    private DrawerLayout drawer;
+    private ListView leftDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerArrowDrawable drawerArrow;
+    private String[] leftSliderData = {"Home", "Android", "Tech Zone", "Sitemap", "About", "Contact Me"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addressbook);
+
+        init();
+        initDrawer();
         session = new SessionManager(getApplicationContext());
 
         AddressList = (ListView) findViewById(R.id.ListViewAddress);
@@ -47,8 +66,70 @@ public class AddressBook extends Activity {
             }
         });
         new AddressTask(getApplicationContext(),addressess,session.getUserDetails().getApi_token()).execute();
-        //addressess = (ArrayList<Address>) getIntent().getSerializableExtra("addressbook");
     }
+
+    private void init() {
+        getActionBar().setIcon(new ColorDrawable(Color.TRANSPARENT));
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        leftDrawerList = (ListView) findViewById(R.id.left_drawer);
+        navigationDrawerAdapter=new ArrayAdapter<String>( AddressBook.this,android.R.layout.simple_list_item_1, leftSliderData);
+        leftDrawerList.setAdapter(navigationDrawerAdapter);
+        leftDrawerList.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+        drawer.closeDrawers();
+        Toast.makeText(AddressBook.this, leftSliderData[position].toString() + "", Toast.LENGTH_SHORT).show();
+    }
+
+
+
+    private void initDrawer() {
+        getActionBar().setHomeButtonEnabled(true);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        drawerArrow = new DrawerArrowDrawable(this) {
+            @Override
+            public boolean isLayoutRtl() {
+                return false;
+            }
+        };
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, drawer,drawerArrow, R.string.drawer_open,R.string.drawer_close) {
+
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+        };
+        drawer.setDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return false;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerToggle.onOptionsItemSelected(item);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
     class AddressTask extends AsyncTask<String, Void, Boolean> {
 
         private Context context;
